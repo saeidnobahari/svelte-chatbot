abd064ddd97aab6190b5fb210afd29160a4c374d
saeid nobahari <saeidnobahari1@gmail.com>
2023-01-16 06:03:04 -0800

    create header component

---
+++ src/routes/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/svelte-logo.svg';
+	import github from '$lib/images/github.svg';
+</script>
+
+<header>
+	
+	<h1>AI Chatbot</h1>
+		
+</header>
+
+<style>
+	header {
+		display: flex;
+		padding: 1.5rem;
+	}
+	h1 {
+		margin-left: 84px;
+		color: white;
+		font-size: 28px;
+		font-weight: bold;
+	}
+</style>
